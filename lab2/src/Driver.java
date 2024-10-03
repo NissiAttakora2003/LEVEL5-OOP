dc1bf6513275eeef86d75c1ba4a9c58290bc9dbe
NissiAttakora2003 <146092409+NissiAttakora2003@users.noreply.github.com>
2024-10-03 18:12:46 +0100

    lab 2

---
+++ lab2/src/Driver.java
@@ -0,0 +1,13 @@
+
+public class Driver {
+
+	public static void main(String[] args)
+	{
+		WordProcessor wordCount= new WordProcessor();
+		wordCount.countWords("nissi is");
+		wordCount.countLetters("nissi       is");
+		
+
+	}
+
+}
